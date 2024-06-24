20a68b7a29873a274abb6e0d4d2d3839e98d9f89
Aitor Palomino <apalomino@domain.com.pe>
2024-06-23 20:56:04 -0500

    Se realizó el consumo de la api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import {Router} from 'express'
+import { TraerPeliculas } from '../services/conexion.mjs'
 const router= Router()
 
 router.get('/',(req, res) => res.render('index',{title:'First Website with Node'}))
@@ -7,4 +8,9 @@ router.get('/about',(req, res) => res.render('about',{title:'About me'}))
 
 router.get('/contact',(req, res) => res.render('contact',{title:'Contact me'}))
 
+router.get('/api/get-peliculas', async(req, res) => {
+    const peliculas= await TraerPeliculas()
+    res.status(200).json(peliculas)
+
+})
 export default router
